e2228b16a374b87e58f66b7c2e8efbf6bf9aa362
yijie4188 <yijie4188@gmail.com>
2020-10-30 20:42:43 +0800

    [fix] (template) 方便webstorm识别模块路径重命名

--- template/base/webStorm.config.js
+++ template/base/webStorm.config.js
@@ -1,6 +1,9 @@
 // let webStorm identify special names
 System.config({
   "paths": {
+    "quick-d": "node_modules/quick-d/src/index.js",
+    "quick-d/lib/*": "node_modules/quick-d/src/*",
+
     "~/*": "./*",
     "@/*": "./src/*"
   }
